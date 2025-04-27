bda627f346f18fbe05ba448a454a63b6e8a252ff
Robee Herrera <bee.herrera215@gmail.com>
2025-04-27 14:33:16 +0000

    Add log script

--- log_time.sh
+++ log_time.sh
@@ -3,3 +3,10 @@
 GITHUB_USER="ARobeeHerrera"
 
 echo "Current date and time: $(date)" >> log.txt
+
+if [ ! -f log.txt ]; then
+    echo "Log File Created at $current_time by $GITHUB_USER" > log.txt
+
+else 
+    update_count=$(wc -1 < log.txt)
+    echo "Log Update #$update_count: $current_time by $GITHUB_USER" >> log.txt
